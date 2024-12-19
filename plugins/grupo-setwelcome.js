d88c17c3090522b9a7cbcaf9c0ce592a50f8be4d
merms2002 <indense7@GMail.com>
2024-12-19 16:59:22 +0200

    Update grupo-setwelcome.js

--- plugins/grupo-setwelcome.js
+++ plugins/grupo-setwelcome.js
@@ -5,7 +5,7 @@ conn.reply(m.chat, lenguajeGB.smsSetW(), fkontak, m)
 //conn.sendButton(m.chat, wm, lenguajeGB['smsSetW'](), null, [[lenguajeGB.smsConMenu(), `/menu`]], fkontak, m)
 } else throw `${lenguajeGB['smsSetW2']()}`
 }
-handler.command = ['setwelcome', 'bienvenida'] 
+handler.command = ['الترحيب', 'bienvenida'] 
 handler.botAdmin = true
 handler.admin = true
 handler.group = true
